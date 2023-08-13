d798bcf8f2f1233eb4bd965202f329a22b5c0e03
Ranjan Mehta <rnjnmhta@gmail.com>
2023-08-13 10:40:09 +0530

    added game logic no ui

---
+++ app.js
@@ -0,0 +1,53 @@
+const choices = ["stone", "paper", "scissors"]
+
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+
+
+let countZero = 0, countOne = 0, draw = 0;
+
+function playRound(computerSelection, playerSelection) {
+    if (playerSelection == computerSelection) { return draw++ }
+
+    if (playerSelection == "stone") {
+        if (computerSelection == "paper") { return ++countZero }
+        else { return ++countOne }
+    }
+
+    if (playerSelection == "paper") {
+        if (computerSelection == "scissors") { return ++countZero }
+        else { return ++countOne }
+    }//good
+
+    if (playerSelection == "scissors") {
+        if (computerSelection == "stone") { return ++countZero }
+        else { return ++countOne }
+    }
+
+}
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("enter").toLowerCase();
+        playRound(computerSelection, playerSelection);
+    }
+
+    if (countOne > countZero) {
+        console.log("Player wins")
+        console.log(`Player wins ${countOne} times ,Computer wins ${countZero} times,game draw ${draw} times`)
+    }
+    else {
+        console.log("Computer wins")
+        console.log(`Player wins ${countOne} times ,Computer wins ${countZero} times,game draw ${draw} times`)
+    }
+    }
+
+
+
+game();
+
